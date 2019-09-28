0e8c59bfa8d12b4a85568625404fe52aaa70aa87
Aman Sarosh <amansarosh13@gmail.com>
2019-09-28 00:45:33 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public auth: AuthService) {}
   title = 'firebase1';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { config } from 'rxjs';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxxxxx3oS9gemdN4", //hidden for obv reasons
+  authDomain: "firebasics1-744a7.firebaseapp.com",
+  databaseURL: "https://firebasics1-744a7.firebaseio.com",
+  projectId: "firebasics1-744a7",
+  storageBucket: "firebasics1-744a7.appspot.com",
+  messagingSenderId: "522715585239",
+  appId: "1:522715585239:web:15df6c8ac74285fe22ae92",
+  measurementId: "G-6T3Z41CGPE"
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
