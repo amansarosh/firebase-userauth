f75e54e05aa10e21a71d82565ea2e1ff55971ffe
Aman Sarosh <amansarosh13@gmail.com>
2020-05-31 19:10:57 -0700

    Formatting Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const firebaseConfig = {
     AngularFireStorageModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
